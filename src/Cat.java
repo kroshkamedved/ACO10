3f29bc2748d4e53878dfd516f7af4ef7a295e83f
kroshkamedved <lokhmytko.gavriil@gmail.com>
2015-12-06 17:17:43 +0200

    first commit

---
+++ src/Cat.java
@@ -0,0 +1,29 @@
+/**
+ * Created by gavri on 06.12.2015.
+ */
+public class Cat {
+    String name;
+    int age;
+    String kind;
+
+    public Cat(){
+
+    }
+
+    public  Cat(String name,int age, String kind ) {
+        this.name = name;
+        this.age = age;
+        this.kind = kind;
+    }
+
+    public String sayMeow(){
+        return String.format("Hello, my name is \"%s\", I'm %d years old, my kind is %s\n"
+                ,name,age,kind);
+
+    }
+        @Override
+    public  String toString(){
+            return String.format("Hello, my name is \"%s\", I'm %d years old, my kind is %s\n"
+                    ,name,age,kind);
+        }
+}
