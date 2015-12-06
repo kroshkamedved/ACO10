ba109bebca3225f3089995bd58950719d4df5791
kroshkamedved <lokhmytko.gavriil@gmail.com>
2015-12-06 18:11:56 +0200

    add it

---
+++ src/homework/TestMyInteger.java
@@ -0,0 +1,12 @@
+package homework;
+
+/**
+ * Created by gavri on 06.12.2015.
+ */
+public class TestMyInteger {
+    public static void main(String[] args) {
+        MyInteger num1 = new MyInteger(89);
+        int sum = num1.add(11);
+        System.out.println(sum);
+    }
+}
