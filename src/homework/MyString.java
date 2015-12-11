f56b88f76136771e8b7b443928ef5d10f1566fe2
kroshkamedved <lokhmytko.gavriil@gmail.com>
2015-12-11 22:43:07 +0200

    Conact

--- src/homework/MyString.java
+++ src/homework/MyString.java
@@ -4,38 +4,46 @@ package homework;
  * Created by gavri on 10.12.2015.
  */
 public class MyString {
-    String content = "";
+    char[] content;
 
     //Конструкторы:
-    //1
-    public MyString(char[] array) {
-        for (int i = 0; i < array.length; i++) {
-            content = content + array[i];
-        }
 
+    public MyString(char[] array) {
+        content = array;
     }
 
-    // 2
+
     public MyString() {
         return;
-    }
-
-    //2
-    public MyString(String content) {
-        this.content = content;
     }
 
 
     // Методы
 
     public void showContent() {
-        System.out.println(content);
+        for (int i = 0; i < content.length; i++) {
+            System.out.print(content[i]);
+        }
     }
 
-    public void concatenation(String newStringEnding) {
-        content = content + newStringEnding;
-    }
+    public MyString concatenate(char[] newStringEnding) {
+        char[] newcontent = new char[content.length + newStringEnding.length];
+        int i = 0;
+        int length = content.length;
+            while (i < content.length) {
+                newcontent[i] = content[i];
+                i++;
 
+            }
+            while (i < newcontent.length) {
+                newcontent[i] = newStringEnding[i-length];
+                i++;
+            }
+
+
+        return  new MyString(newcontent);
+    }
+/*
 
     public void toLowerCase() {
         content = content.toLowerCase();
@@ -53,12 +61,12 @@ public class MyString {
 
             for (int i = 0; i < content.length(); i++) {
 
-                if (textArray[0] == contentArray[i]){
+                if (textArray[0] == contentArray[i]) {
 
-                    for (int j = 0; j < textArray.length && textArray[j] == contentArray[i+j];j++ )
-                    {
-                        if(j == textArray.length-1){
-                            System.out.println ("From - " + i + " to - "+ (i+j));}
+                    for (int j = 0; j < textArray.length && textArray[j] == contentArray[i + j]; j++) {
+                        if (j == textArray.length - 1) {
+                            System.out.println("From - " + i + " to - " + (i + j));
+                        }
 
                     }
                 }
@@ -66,5 +74,5 @@ public class MyString {
             }
         }
 
-    }
+    }*/
 }
--- src/homework/TestMyString.java
+++ src/homework/TestMyString.java
@@ -1,32 +1,17 @@
 package homework;
 
+import com.sun.deploy.util.ArrayUtil;
+
 /**
  * Created by gavri on 10.12.2015.
  */
 public class TestMyString {
     public static void main(String[] args) {
-        char[] array = {'s','d','m','n'};
-         MyString anyString = new MyString(array);
-        System.out.println(anyString.content);
-
-        MyString anyString3 = new MyString();
-        anyString3.content = "Baradaada x";
-        System.out.println(anyString3.content);
-
-        MyString anyString4 = new MyString("Barada xy");
-        System.out.println(anyString4.content);
-
-        anyString4.showContent();
-
-        anyString.concatenation("la-la-la");
-        anyString.toUpperCAse();
-        anyString.showContent();
-        anyString.toLowerCase();
-        anyString.showContent();
-        anyString3.searchingText("ada");
-
-
-
+        char[] array = {'d','d','f','h'};
+        char[] array2 = {'a','d','f','h'};
+        MyString anyString = new MyString(array);
+        MyString result = anyString.concatenate(array2);
+        result.showContent();
 
 
 
