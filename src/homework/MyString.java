e6db9cb820cfd9e8b27efe73d771fce6eebebebb
kroshkamedved <lokhmytko.gavriil@gmail.com>
2015-12-12 01:51:38 +0200

    DeleteSpace

--- src/homework/MyString.java
+++ src/homework/MyString.java
@@ -24,48 +24,71 @@ public class MyString {
         for (int i = 0; i < content.length; i++) {
             System.out.print(content[i]);
         }
+        System.out.println();
     }
 
     public MyString concatenate(char[] newStringEnding) {
-        char[] newcontent = new char[content.length + newStringEnding.length];
+        char[] newContent = new char[content.length + newStringEnding.length];
         int i = 0;
         int length = content.length;
-            while (i < content.length) {
-                newcontent[i] = content[i];
-                i++;
+        while (i < content.length) {
+            newContent[i] = content[i];
+            i++;
 
-            }
-            while (i < newcontent.length) {
-                newcontent[i] = newStringEnding[i-length];
-                i++;
-            }
+        }
+        while (i < newContent.length) {
+            newContent[i] = newStringEnding[i - length];
+            i++;
+        }
 
 
-        return  new MyString(newcontent);
+        return new MyString(newContent);
     }
-/*
+
 
     public void toLowerCase() {
-        content = content.toLowerCase();
+        for (int i = 0; i < content.length; i++) {
+            content[i] = Character.toLowerCase(content[i]);
+        }
     }
 
-    public void toUpperCAse() {
-        content = content.toUpperCase();
+    public void toUpperCase() {
+        for (int i = 0; i < content.length; i++) {
+            content[i] = Character.toUpperCase(content[i]);
+        }
     }
 
-    public void searchingText(String text) {
 
-        char[] textArray = text.toCharArray();
-        char[] contentArray = content.toCharArray();
-        if (textArray.length < contentArray.length) {
+    public void searchWhereIsText(char[] textArray) {
+        if (textArray.length < content.length) {
 
-            for (int i = 0; i < content.length(); i++) {
+            for (int i = 0; i < content.length; i++) {
 
-                if (textArray[0] == contentArray[i]) {
+                if (textArray[0] == content[i]) {
 
-                    for (int j = 0; j < textArray.length && textArray[j] == contentArray[i + j]; j++) {
+                    for (int j = 0; j < textArray.length && textArray[j] == content[i + j]; j++) {
                         if (j == textArray.length - 1) {
-                            System.out.println("From - " + i + " to - " + (i + j));
+                            System.out.println(" You can find your substring in cells from - " + i + " to - " + (i + j));
+                        }
+
+                    }
+                }
+
+            }
+        }
+    }
+
+    public boolean searchText(char[] textArray) {
+        boolean result = false;
+        if (textArray.length < content.length) {
+
+            for (int i = 0; i < content.length; i++) {
+
+                if (textArray[0] == content[i]) {
+
+                    for (int j = 0; j < textArray.length && textArray[j] == content[i + j]; j++) {
+                        if (j == textArray.length - 1) {
+                            result = true;
                         }
 
                     }
@@ -74,5 +97,55 @@ public class MyString {
             }
         }
 
-    }*/
+        return result;
+    }
+
+
+    public MyString subMyString(int first, int last) {
+        if (first > last | content.length < last) {
+            System.out.println("Error - Write correct range");
+        }
+        char[] array = new char[(content.length - first) - (content.length - last) + 1];
+        MyString newMyString = new MyString(array);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = content[i + first];
+        }
+
+        return newMyString;
+    }
+
+    public MyString deleteSpace() {
+        int j = 0;
+        int i = 0;
+        while (i < content.length) {
+            if (content[i] == ' ') {
+                j++;
+                i++;
+            }else{
+                i++;
+            }
+        }
+
+        char[] array = new char[content.length - j];
+        j = 0;
+        i=0;
+
+
+        while (i < content.length) {
+            if (content[i] != ' ') {
+                array[j] = content[i];
+                j++;
+                i++;
+            } else {
+                i++;
+            }
+        }
+
+        MyString newString = new MyString(array);
+
+        return newString;
+
+    }
+
+
 }
--- src/homework/TestMyString.java
+++ src/homework/TestMyString.java
@@ -7,13 +7,38 @@ import com.sun.deploy.util.ArrayUtil;
  */
 public class TestMyString {
     public static void main(String[] args) {
-        char[] array = {'d','d','f','h'};
-        char[] array2 = {'a','d','f','h'};
-        MyString anyString = new MyString(array);
-        MyString result = anyString.concatenate(array2);
+        char[] array = {'d','D','f','h'};
+        char[] array2 = {'D'};
+            MyString anyString = new MyString(array);
+            MyString result = anyString.concatenate(array2);
+                result.showContent();
+        result.toLowerCase();
+            System.out.println();
         result.showContent();
+            System.out.println();
+        result.toUpperCase();
+        result.showContent();
+            System.out.println();
 
+        boolean a = anyString.searchText(array2);
+            System.out.println(a);
+
+        MyString result2 = result.subMyString(0,2);
+            System.out.println();
+            result2.showContent();
+        System.out.println();
+
+        char[] chatArray = {' ', 's','s','X',' ','!'};
+
+        MyString tryNewMeth = new MyString(chatArray);
+            tryNewMeth.showContent();
+            System.out.println();
+        MyString tryNewMeth2 = tryNewMeth.deleteSpace();
+            tryNewMeth2.showContent();
 
 
     }
+
+
+
 }
