6829d4792d497d21e3c79be18ea98d2160a04fff
kroshkamedved <lokhmytko.gavriil@gmail.com>
2016-01-23 15:17:31 +0200

    BinarySearch add;findMin&Max: clear

--- src/TreeSet/BinarySearchTree.java
+++ src/TreeSet/BinarySearchTree.java
@@ -1,8 +1,10 @@
 package TreeSet;
 
 import java.util.Iterator;
+import java.util.Iterator;
 
-public class BinarySearchTree<T> implements ITree<T> {
+/*
+public class BinarySearchTree<T extends Comparable> implements ITree<T> {
     private Node<T> root;
     private int size;
 
@@ -57,6 +59,22 @@ public class BinarySearchTree<T> implements ITree<T> {
         return true;
     }
 
+
+    public Node<T> binarySearch(T t){
+        Node<T> iter = root;
+        while (iter != null){
+             int compare = t.compareTo(iter.value);
+            if (compare < 0){
+                iter = iter.leftChild;
+            } else if (compare > 0) {
+               iter = iter.rightChild;
+            } else {
+                break;
+            }
+        }
+        return  iter;
+    }
+
     @Override
     public boolean remove(T t) {
         return false;
@@ -74,6 +92,7 @@ public class BinarySearchTree<T> implements ITree<T> {
 
     @Override
     public T findMin() {
+        if(root==null) return null;
         Node<T> iter = root;
         while (iter.leftChild!=null) iter = iter.leftChild;
         return iter.value;
@@ -81,6 +100,7 @@ public class BinarySearchTree<T> implements ITree<T> {
 
     @Override
     public T findMax() {
+        if(root==null) return null;
         Node<T> iter = root;
         while (iter.rightChild!=null) iter = iter.rightChild;
         return iter.value;
@@ -117,6 +137,170 @@ public class BinarySearchTree<T> implements ITree<T> {
         public Node(E value) {
             this.value = value;
         }
+
+    }
+}
+*/
+
+public class BinarySearchTree<T extends Comparable<T>> implements ITree<T> {
+    private Node<T> root;
+    private int size;
+
+    @Override
+    public boolean add(T obj) {
+        if(root == null) {
+            root = new Node<>(obj);
+            size++;
+            return true;
+        }
+        Node<T> iter = root;
+        while(true){
+            int compare = obj.compareTo(iter.value);
+            if(compare < 0){
+                if(iter.leftChild == null){
+                    iter.leftChild = new Node<>(obj, iter);
+                    size++;
+                    return true;
+                } else{
+                    iter = iter.leftChild;
+                }
+            }else if(compare > 0){
+                if(iter.rightChild == null){
+                    iter.rightChild = new Node<>(obj, iter);
+                    size++;
+                    return true;
+                } else{
+                    iter = iter.rightChild;
+                }
+            } else{
+                return false;
+            }
+        }
+    }
+
+    @Override
+    public boolean remove(T t) {
+        Node<T> delete = binarySearch(t);
+        if(delete.leftChild == null && delete.rightChild == null){
+            if(delete == root){
+                root = null;
+            } else if(delete == delete.parent.leftChild){
+                delete.parent.leftChild = null;
+            } else {
+                delete.parent.rightChild = null;
+            }
+            size--;
+            return true;
+        } else if(delete.leftChild == null || delete.rightChild == null){
+            Node<T> child = delete.leftChild == null ? delete.rightChild : delete.leftChild;
+            child.parent = delete.parent;
+            if(delete == root){
+                root = child;
+            } else if (delete == delete.parent.leftChild){
+                delete.parent.leftChild = child;
+            } else {
+                delete.parent.rightChild = child;
+            }
+            size--;
+            return true;
+
+        } else{
+            Node<T> iter = delete.rightChild;
+            while (iter.leftChild != null){
+                iter = iter.leftChild;
+            }
+            if(iter.rightChild!= null){
+                iter.rightChild.parent = iter.parent;
+                iter.parent.leftChild = iter.rightChild;
+            }
+            delete.rightChild.parent = iter;
+            delete.leftChild.parent = iter;
+            delete.parent.rightChild = iter;
+            iter = null;
+            size--;
+            return true;
+        }
+    }
+
+    @Override
+    public boolean contains(T t) {
+
+        Node<T> search = binarySearch(t);
+        return search != null;
+    }
+
+    private Node<T> binarySearch(T obj) {
+        Node<T> iter = root;
+        while(iter != null){
+            int compare = obj.compareTo(iter.value);
+            if(compare < 0) {
+                iter = iter.leftChild;
+            }else if(compare > 0){
+                iter = iter.rightChild;
+            } else{
+                break;
+            }
+        }
+        return iter;
+    }
+
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T findMin() {
+        if(root == null){
+            return null;
+        }
+        Node<T> iter = root;
+        while (iter.leftChild != null){
+            iter = iter.leftChild;
+        }
+        return iter.value;
+    }
+
+    @Override
+    public T findMax() {
+        if(root == null){
+            return null;
+        }
+        Node<T> iter = root;
+        while (iter.rightChild != null){
+            iter = iter.rightChild;
+        }
+        return iter.value;
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    private class Node<E> {
+        private E value;
+        Node<E> parent;
+        Node<E> leftChild ;
+        Node<E> rightChild;
+
+
+        public Node(E value, Node<E> parent) {
+            this.value = value;
+            this.parent = parent;
+        }
+
+        public Node(E value) {
+            this.value = value;
+        }
+
 
     }
 }
--- src/TreeSet/TestTree.java
+++ src/TreeSet/TestTree.java
@@ -11,13 +11,24 @@ public class TestTree {
         tree.add(20);
         tree.add(5);
         tree.add(7);
-        tree.add(0);
+        //tree.add(0);
         System.out.println(tree.size());
         System.out.println(tree.findMin());
         System.out.println(tree.findMax());
-        tree.clear();
+        //tree.clear();
         tree.add(77);
+       // System.out.println(tree.size());
+        tree.add(20);
+       // System.out.println(tree.remove(20));
         System.out.println(tree.size());
-
+        tree.add(90);
+        tree.add(56);
+        tree.add(59);
+        tree.add(55);
+        tree.add(99);
+        tree.add(89);
+        System.out.println(tree.size());
+        System.out.println(tree.remove(77));
+        System.out.println(tree.size());
     }
 }
